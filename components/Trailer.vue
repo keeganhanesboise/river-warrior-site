29f0141b845fef7911cc2cb6186ce10332efa204
keeganhanesboise <keeganhanes5@gmail.com>
2025-05-27 21:24:05 -0700

    added trailer header and logline

--- components/Trailer.vue
+++ components/Trailer.vue
@@ -1,13 +1,23 @@
 <template>
   <section>
-    <h1 class="sr-only">Watch Trailer</h1>
     <div class="video-container-primary">
-      <div class="video-container-secondary">
-        <iframe
-          allowfullscreen
-          loading="lazy"
-          src="https://www.youtube.com/embed/zjwntAmkrN0"
-          title="River Warrior Trailer" />
+      <div class="content-wrapper">
+        <div class="text-content">
+          <h1 class="title">WATCH THE TRAILER</h1>
+          <p class="logline">
+            An inspiring agricultural adventure documentary about an
+            Appalachian, off-grid farmer who is going full force into her next
+            adventure - kayaking from her neighborhood creek all the way to the
+            Gulf of Mexico
+          </p>
+        </div>
+        <div class="video-container-secondary">
+          <iframe
+            allowfullscreen
+            loading="lazy"
+            src="https://www.youtube.com/embed/zjwntAmkrN0"
+            title="River Warrior Trailer" />
+        </div>
       </div>
     </div>
   </section>
@@ -22,10 +32,33 @@
   padding: 5rem 2rem;
 }
 
-@media (min-width: 640px) {
-  .video-container-primary {
-    padding: 8rem 3rem;
-  }
+.content-wrapper {
+  width: 100%;
+  max-width: 1280px;
+  margin: 0 auto;
+}
+
+.text-content {
+  text-align: center;
+  color: white;
+  margin-bottom: 3rem;
+  max-width: 900px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.title {
+  font-size: 2.5rem;
+  margin: 0 0 1.5rem 0;
+  letter-spacing: 0.05em;
+  font-weight: normal;
+}
+
+.logline {
+  font-size: 1.25rem;
+  line-height: 1.6;
+  margin: 0;
+  opacity: 0.9;
 }
 
 .video-container-secondary {
@@ -36,7 +69,7 @@
 }
 
 .video-container-secondary::before {
-  content: "";
+  content: '';
   display: block;
   padding-bottom: 56.25%; /* 16:9 aspect ratio */
 }
@@ -48,5 +81,23 @@
   width: 100%;
   height: 100%;
   border: none;
+}
+
+@media (min-width: 640px) {
+  .video-container-primary {
+    padding: 8rem 3rem;
+  }
+
+  .title {
+    font-size: 3.5rem;
+  }
+
+  .logline {
+    font-size: 1.5rem;
+  }
+
+  .text-content {
+    margin-bottom: 4rem;
+  }
 }
 </style>
