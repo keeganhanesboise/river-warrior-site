327fbfb0819febeb844633de6aa9dfae9451a2f8
keeganhanesboise <keeganhanes5@gmail.com>
2025-05-28 15:04:41 -0700

    added favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,27 @@ export default defineNuxtConfig({
         lang: 'en'
       },
       title: 'River Warrior Documentary',
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: '/favicon-96x96.png',
+          sizes: '96x96'
+        },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'shortcut icon', href: '/favicon.ico' },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png'
+        },
+        { rel: 'manifest', href: '/site.webmanifest' }
+      ],
+      meta: [
+        { name: 'theme-color', content: '#ffffff' },
+        { name: 'apple-mobile-web-app-capable', content: 'yes' },
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'default' }
+      ]
     }
   }
 });
